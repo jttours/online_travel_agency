26504138b41dab46343ac7a0e3d8f6c50dd76b7b
jttours <omar@jttours.com>
2021-05-07 14:40:01 +0300

    bootstrapping

---
+++ ota-server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.username,
+    password: process.env.password,
+    database: process.env.database
+  });
+
+
+
+
+
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
